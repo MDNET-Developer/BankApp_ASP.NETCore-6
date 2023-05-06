f402a82558ba7e5eb375f0498c85223fd08d53e3
MDNET-Developer <qarabagli.murad@mail.ru>
2023-05-06 20:30:02 +0400

    A bit of update in AppUserRegisterValidator class

--- BankAppIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
+++ BankAppIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -12,14 +12,16 @@ namespace BankAppIdentityProject.BusinessLayer.ValidationRules.AppUserValidation
     {
         public AppUserRegisterValidator()
         {
-            RuleFor(x=>x.Name).NotEmpty().WithMessage("Verilən daxil edin !!!");
-            RuleFor(x=>x.SurName).NotEmpty().WithMessage("Verilən daxil edin !!!");
-            RuleFor(x=>x.UserName).NotEmpty().WithMessage("Verilən daxil edin !!!");
-            RuleFor(x=>x.Email).NotEmpty().WithMessage("Verilən daxil edin !!!");
-            RuleFor(x=>x.Password).NotEmpty().WithMessage("Verilən daxil edin !!!");
-            RuleFor(x=>x.ConfirmPassword).NotEmpty().WithMessage("Verilən daxil edin !!!");
+            RuleFor(x=>x.Name).NotEmpty().WithMessage("Adınızı daxil edin !!!");
+            RuleFor(x=>x.SurName).NotEmpty().WithMessage("Soyadınızı daxil edin !!!");
+            RuleFor(x=>x.UserName).NotEmpty().WithMessage("İstifadəçi adını daxil edin !!!");
+            RuleFor(x=>x.Email).NotEmpty().WithMessage("Elektron poçtu daxil edin !!!");
+            RuleFor(x=>x.Password).NotEmpty().WithMessage("Şifrəni daxil edin !!!");
+            RuleFor(x=>x.ConfirmPassword).NotEmpty().WithMessage("Təkrar şifrəni daxil edin !!!");
             RuleFor(x => x.Name).MaximumLength(30).WithMessage("Ən az 30 simvol daxil edilə bilər !!!");
             RuleFor(x => x.Name).MinimumLength(3).WithMessage("Ən az 3 simvol daxil edilə bilər !!!");
+            RuleFor(x => x.ConfirmPassword).Equal(x=>x.Email).WithMessage("Təkrar şifrə ilə şifrə uyğun deyil !!!");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Elektron poçt formatını düzgün daxil edin !!!");
         }
     }
 }
